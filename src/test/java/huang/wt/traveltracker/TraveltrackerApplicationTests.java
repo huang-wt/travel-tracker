da763728f8be2e62f5a7a8838ed48b7a60920d95
huang-wt <314936302@qq.com>
2024-02-22 18:51:12 +0000

    init spring boot project

---
+++ src/test/java/huang/wt/traveltracker/TraveltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package huang.wt.traveltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
