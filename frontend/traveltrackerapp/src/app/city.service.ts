1bae3ccca2c5f6d02ae820fca60be6cd873af3c5
huang-wt <314936302@qq.com>
2024-02-23 15:35:27 +0000

    created city service with http client calling rest api

---
+++ frontend/traveltrackerapp/src/app/city.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { City } from './city';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CityService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getCities(): Observable<City[]> {
+    return this.http.get<City[]>(`${this.apiServerUrl}/city/all`);
+  }
+
+  public addCity(city: City): Observable<City> {
+    return this.http.post<City>(`${this.apiServerUrl}/city/add`, city);
+  }
+
+  public updateCity(city: City): Observable<City> {
+    return this.http.put<City>(`${this.apiServerUrl}/city/update`, city);
+  }
+
+  public deleteCity(cityId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/city/delete/${cityId}`);
+  }
+}
