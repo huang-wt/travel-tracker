628bb3e7996bc858c29dbd73f61a3e18ea0b592f
huang-wt <314936302@qq.com>
2024-02-23 15:36:11 +0000

    added get cities method

--- frontend/traveltrackerapp/src/app/app.component.ts
+++ frontend/traveltrackerapp/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { City } from './city';
+import { CityService } from './city.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'traveltrackerapp';
+
+  public cities: City[] = [];
+
+  constructor(private cityService: CityService){}
+
+  ngOnInit(): void {
+    this.getCities();
+  }
+
+  public getCities(): void {
+    this.cityService.getCities().subscribe(
+      (response: City[]) => {
+        this.cities = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
 }
