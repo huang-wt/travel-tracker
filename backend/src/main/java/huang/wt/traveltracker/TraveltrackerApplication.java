e5f3129424eba819cc5932e87d9c7249cbab13c4
huang-wt <314936302@qq.com>
2024-02-23 13:31:36 +0000

    moved backend project to backend folder

--- backend/src/main/java/huang/wt/traveltracker/TraveltrackerApplication.java
+++ backend/src/main/java/huang/wt/traveltracker/TraveltrackerApplication.java
@@ -2,12 +2,16 @@ package huang.wt.traveltracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TraveltrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TraveltrackerApplication.class, args);
 	}
-
 }
